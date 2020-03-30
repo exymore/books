e316a39a04542e238dbc3b8d7b2d731df7c62894
Mikita Tserakhau <exymore@yandex.ru>
2020-03-30 19:57:40 +0300

    WIP: Reader pages

--- components/Reader.vue
+++ components/Reader.vue
@@ -1,11 +1,41 @@
 <template>
-  <div
-    class="reader-wrapper"
-    v-html="readerHtml"
-  />
+  <div class="container">
+    <v-btn
+      color="cyan lighten-4"
+      fab
+      large
+      @click="prev"
+      class="btn"
+    >
+      <v-icon
+        class="white--text"
+      >
+        {{ 'mdi-chevron-left' }}
+      </v-icon>
+    </v-btn>
+    <div
+      class="reader-wrapper"
+      v-html="readerHtml"
+    />
+    <v-btn
+      color="cyan lighten-4"
+      fab
+      large
+      @click="next"
+      class="btn"
+    >
+      <v-icon
+        class="white--text"
+      >
+        {{ 'mdi-chevron-right' }}
+      </v-icon>
+    </v-btn>
+  </div>
 </template>
 
 <script>
+  import debounce from 'lodash/debounce';
+
   export default {
     name: 'Reader',
     props: {
@@ -14,21 +44,62 @@
         default: '',
       },
     },
+    data: () => ({
+      columnWidth: 0,
+      el: null,
+    }),
     mounted() {
+      this.el = document.querySelector('.reader-wrapper');
+      this.columnWidth = this.el.offsetWidth;
+      this.el.style.columnWidth = this.columnWidth/2
     },
-  }
+    methods: {
+      next() {
+        console.log(this.el.scrollLeft);
+        console.log(this.columnWidth);
+        this.el.scroll({
+          top: 0,
+          left: this.el.scrollLeft + this.columnWidth + 16,
+          behavior: 'smooth',
+        });
+      },
+      prev() {
+        console.log(this.el.scrollLeft);
+        console.log(this.columnWidth);
+        this.el.scroll({
+          top: 0,
+          left: this.el.scrollLeft - this.columnWidth - 16,
+          behavior: 'smooth',
+        });
+      },
+    },
+  };
 </script>
 
 <style scoped>
+  .btn:first-child {
+    margin-right: 48px;
+  }
+  .btn:last-child {
+    margin-left: 48px;
+  }
+  .container {
+    width: 100%;
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    justify-content: center;
+  }
+
   .reader-wrapper {
-    margin-left: auto;
-    margin-right: auto;
-    width: 70vw;
     word-break: break-word;
-    column-width: 100%;
-    height: calc(90vh - 64px);
-    overflow-x: auto;
+    height: 80vh;
+    width: 40vw;
+    column-fill: balance;
+    column-span: all;
+    overflow-x: hidden;
     column-count: 2;
+    column-width: 35vw;
     display: block;
   }
 </style>
