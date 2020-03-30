da78f8145be3d9230fecabcca7af4dfe802c809f
Mikita Tserakhau <exymore@yandex.ru>
2020-03-30 15:20:51 +0300

    Add HTML book parsing

--- pages/read/_slug.vue
+++ pages/read/_slug.vue
@@ -1,6 +1,6 @@
 <template>
   <v-container>
-    <div />
+    <div v-html="bookHTML.value" />
   </v-container>
 </template>
 
@@ -16,20 +16,22 @@
     data() {
       return {
         book: null,
+        bookHTML: {id:null,value:null},
       };
     },
     async mounted() {
       this.book = this.$store.state.books.find(book => book.sys.id === this.$route.params.slug);
       const bookFromDB = await DBApi.getByKey(this.book.sys.id);
       if (!bookFromDB) {
-        const storageRef = this.$fireStorage.ref().child('mark-tven-the-adventures-of-huck/index.html');
-        const bookURL = await storageRef.getDownloadURL();
-        const bookHTML = await this.getBook(bookURL);
-        await DBApi.save({ id: this.book.sys.id, value: bookHTML });
+        const procceededHTML = await this.getHTMLUrl()
+            .then(URL => this.downloadBook(URL))
+            .then(HTML => this.processDocument(HTML));
+        await DBApi.save({ id: this.book.sys.id, value: procceededHTML });
       }
+      this.bookHTML = await DBApi.getByKey(this.book.sys.id);
     },
     methods: {
-      async getBook(bookURL) {
+      async downloadBook(bookURL) {
         const options = {
           method: 'GET',
           url: bookURL,
@@ -37,13 +39,44 @@
         const { data } = await axios(options);
         return data;
       },
-      processDocument() {
-
+      async getHTMLUrl() {
+        const storageHTMLRef = this.$fireStorage.ref().child(`${this.book.fields.bookUrl}/index.html`);
+        const bookURL = await storageHTMLRef.getDownloadURL();
+        return bookURL;
       },
+      async getCSSUrl() {
+        const storageStylingRef = this.$fireStorage.ref().child(`${this.book.fields.bookUrl}/style.css`);
+        const cssUrl = await storageStylingRef.getDownloadURL();
+        return cssUrl;
+      },
+      async getImageUrl(imgName) {
+        const storageImageRef = this.$fireStorage.ref().child(`${this.book.fields.bookUrl}/images/${imgName}`);
+        const imgUrl = await storageImageRef.getDownloadURL();
+        return imgUrl;
+      },
+      async processDocument(HTML) {
+        const srcRegex = new RegExp('src\s*=\s*"(.+?)"', "g");
+        const replaceSrc = async match => {
+          const imgName = match.slice(0, -1).replace('src="images/', '');
+          const imageUrl = await this.getImageUrl(imgName);
+          return match.replace(`images/${imgName}`, imageUrl);
+        }
+
+        const HTMLWithInjectedStyles = HTML.replace('href="style.css"', `href="${await this.getCSSUrl()}"`);
+        return await this.replaceAsync(HTMLWithInjectedStyles, srcRegex, replaceSrc)
+      },
+      async replaceAsync(str, regex, fn) {
+          const promises = [];
+          str.replace(regex, (match, ...args) => {
+          const promise = fn(match, ...args);
+          promises.push(promise);
+        });
+        const data = await Promise.all(promises);
+        return str.replace(regex, () => data.shift());
     },
-  };
+  },
+}
 </script>
 
 <style scoped>
-
 </style>
